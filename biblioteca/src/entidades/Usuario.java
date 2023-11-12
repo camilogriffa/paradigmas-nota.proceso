00e4f1a1b9e60264ad449dbae71be187f65f9cf3
cgriffa <c4milogriffa@gmail.com>
2023-11-12 17:13:58 -0300

    Entidad Usuario

--- biblioteca/src/entidades/Usuario.java
+++ biblioteca/src/entidades/Usuario.java
@@ -1,5 +1,27 @@
 package entidades;
 
 public class Usuario {
-  
+  public int id;
+  public String nombre;
+
+  public Usuario(int id, String nombre) {
+    this.id = id;
+    this.nombre = nombre;
+  }
+
+  public int getId() {
+    return this.id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getNombre() {
+    return this.nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
 }
